0a407b45a0672c237fd075ac9be63a18428a1179
JoeMilius <joemilius@gmail.com>
2021-08-09 15:44:37 -0600

    Completed Assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,19 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    if(params[:name])
+    # students = Student.where('first_name = ?', params[:name].capitalize).or(Student.where('last_name = ?', params[:name].capitalize))
+
+    students = Student.where('first_name = ? or last_name = ?', params[:name].capitalize, params[:name].capitalize)
+    else
+      students = Student.all
+    end
     render json: students
+  end
+
+  def student_record
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
